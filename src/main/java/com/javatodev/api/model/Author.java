3f4491cadf2cf51924a67664afb62e9c34d6cbb8
Ramazan Sakin <ramazansakin63@gmail.com>
2022-02-18 08:35:49 +0300

    feat: refactoring & typo fix

--- src/main/java/com/javatodev/api/model/Author.java
+++ src/main/java/com/javatodev/api/model/Author.java
@@ -1,17 +1,15 @@
 package com.javatodev.api.model;
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
+@Data
 @Document
 public class Author {
     @Id
     private String id;
     private String firstName;
     private String lastName;
+
 }
