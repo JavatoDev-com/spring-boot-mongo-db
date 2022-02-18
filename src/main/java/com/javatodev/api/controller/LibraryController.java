fb4840fb7d36a0d5a197a4240612fb37956f782a
Ramazan Sakin <ramazansakin63@gmail.com>
2022-02-18 08:38:22 +0300

    feat: controller refactoring

--- src/main/java/com/javatodev/api/controller/LibraryController.java
+++ src/main/java/com/javatodev/api/controller/LibraryController.java
@@ -8,21 +8,23 @@ import com.javatodev.api.model.request.BookCreationRequest;
 import com.javatodev.api.model.request.BookLendRequest;
 import com.javatodev.api.model.request.MemberCreationRequest;
 import com.javatodev.api.service.LibraryService;
-
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
-import lombok.RequiredArgsConstructor;
-
+@Validated
 @RestController
 @RequestMapping(value = "/api/library")
 @RequiredArgsConstructor
 public class LibraryController {
     private final LibraryService libraryService;
 
-    @GetMapping("/book")
+    @GetMapping("/books")
     public ResponseEntity readBooks(@RequestParam(required = false) String isbn) {
         if (isbn == null) {
             return ResponseEntity.ok(libraryService.readBooks());
@@ -31,48 +33,51 @@ public class LibraryController {
     }
 
     @GetMapping("/book/{bookId}")
-    public ResponseEntity<Book> readBook (@PathVariable String bookId) {
+    public ResponseEntity<Book> readBook(@PathVariable String bookId) {
         return ResponseEntity.ok(libraryService.readBookById(bookId));
     }
 
     @PostMapping("/book")
-    public ResponseEntity<Book> createBook (@RequestBody BookCreationRequest request) {
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Book> createBook(@RequestBody @Valid BookCreationRequest request) {
         return ResponseEntity.ok(libraryService.createBook(request));
     }
 
     @PatchMapping("/book/{bookId}")
-    public ResponseEntity<Book> updateBook (@PathVariable("bookId") String bookId, @RequestBody BookCreationRequest request) {
+    public ResponseEntity<Book> updateBook(@PathVariable("bookId") String bookId, @RequestBody BookCreationRequest request) {
         return ResponseEntity.ok(libraryService.updateBook(bookId, request));
     }
 
     @PostMapping("/author")
-    public ResponseEntity<Author> createAuthor (@RequestBody AuthorCreationRequest request) {
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Author> createAuthor(@RequestBody @Valid AuthorCreationRequest request) {
         return ResponseEntity.ok(libraryService.createAuthor(request));
     }
 
     @DeleteMapping("/book/{bookId}")
-    public ResponseEntity<Void> deleteBook (@PathVariable String bookId) {
+    public ResponseEntity<Void> deleteBook(@PathVariable String bookId) {
         libraryService.deleteBook(bookId);
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("/member")
-    public ResponseEntity<Member> createMember (@RequestBody MemberCreationRequest request) {
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Member> createMember(@RequestBody @Valid MemberCreationRequest request) {
         return ResponseEntity.ok(libraryService.createMember(request));
     }
 
-    @GetMapping("/member")
-    public ResponseEntity<List<Member>> readMembers () {
+    @GetMapping("/members")
+    public ResponseEntity<List<Member>> readMembers() {
         return ResponseEntity.ok(libraryService.readMembers());
     }
 
     @PatchMapping("/member/{memberId}")
-    public ResponseEntity<Member> updateMember (@RequestBody MemberCreationRequest request, @PathVariable String memberId) {
+    public ResponseEntity<Member> updateMember(@RequestBody @Valid MemberCreationRequest request, @PathVariable String memberId) {
         return ResponseEntity.ok(libraryService.updateMember(memberId, request));
     }
 
     @PostMapping("/book/lend")
-    public ResponseEntity<List<String>> lendABook(@RequestBody BookLendRequest bookLendRequests) {
+    public ResponseEntity<List<String>> lendABook(@RequestBody @Valid BookLendRequest bookLendRequests) {
         return ResponseEntity.ok(libraryService.lendABook(bookLendRequests));
     }
 }
